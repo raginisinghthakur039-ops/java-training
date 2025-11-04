8a4ffe187d301adce9a3348e5c72b63db18eb39b
raginisinghthakur039-ops <raginisinghthakur039@gmail.com>
2025-11-04 20:18:38 +0530

    check odd or even
    check odd or even

---
+++ main1.java
@@ -0,0 +1,14 @@
+class main1 {
+    public static void main(String[] args) {
+        int a=5;
+        if(a%2==0){
+            System.out.print("");
+        }else{
+
+            System.out.println(" odd");
+        }
+
+    }
+}
+
+
