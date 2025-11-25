f1e8ac88023f49d619ee8e04a8944d4585d19ae4
raginisinghthakur039-ops <raginisinghthakur039@gmail.com>
2025-11-25 21:00:25 +0530

    array list
    array list

---
+++ arraylist.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class arraylist {
+    public static void main(String[]args) {
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(10);
+        list.add(15);
+        list.add(20);
+        list.add(0,5);
+        System.out.println(list);
+//        list.remove(0);
+//        list.remove
+//        int size=list.size();
+//        boolean isEmpty =list.isEmpty();
+
+
+    }
+}
