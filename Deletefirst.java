105a8486619bac6f7f176954cc15aea5c128312e
raginisinghthakur039-ops <raginisinghthakur039@gmail.com>
2026-04-10 11:11:15 +0530

    Delete first

---
+++ Deletefirst.java
@@ -0,0 +1,19 @@
+
+
+public class Deletefirst {
+    public static void main(String[] args){
+        String str="Ragini";
+        int count=0;
+        char letter ='R';
+//        for(int i=0;i<str.length();i++){
+//            for(int j=i;j<str.length();j++){
+                String result=str.substring(1);
+                System.out.println("Original String:"+str);
+                System.out.println("After Deleting First Letter:"+result);
+
+
+            }
+
+        }
+//    }
+//}
