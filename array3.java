5cbaccbd2e9705b8d59801166e79c4745d0a36ff
raginisinghthakur039-ops <raginisinghthakur039@gmail.com>
2026-04-22 09:52:21 +0530

    recursive function

---
+++ array3.java
@@ -0,0 +1,25 @@
+public class array3 {
+    public static void main(String[]args){
+        int [][]arr={
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+        };
+        int key = 5;
+        boolean found =false;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if (arr[i][j]==key){
+                    System.out.println("Key found at position:("+i+","+j+")");
+                    found=true;
+                    break;
+
+                }
+            }
+            if(found)break;
+        }
+        if(!found){
+            System.out.println("Key not found");
+        }
+    }
+}
